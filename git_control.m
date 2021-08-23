7ef91059f1d0cece7b22dff99131aec10dd75b47
ZHIBIN ZHOU <zhibinz2@uci.edu>
2021-08-23 16:42:48 -0700

    CommitName

--- git_control.m
+++ git_control.m
@@ -3,5 +3,22 @@
 CommitName=char(datetime('now'));
 !git commit -a -m "CommitName"
 
+!git push
+%%
+% undo all current changes
+!git restore .
 
-% !git push
+%get rid of untracked files
+!git clean -f
+
+%%
+% save changes temporarily
+!git stash --include-untracked
+
+% to restore the stash
+!git stash pop
+
+%% Update the URL of origin remote using SSH instead of HTTPS so that it will not ask for password when push
+!git remote set-url origin git@github.com:zhibinz2/Motor_cordination.git
+
+!git push
