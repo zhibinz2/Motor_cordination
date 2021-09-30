c6adb53af381523662ebfd06925f392b1c8397c9
ZHIBIN ZHOU <zhibinz2@uci.edu>
2021-09-30 16:52:03 -0700

    CommitName

--- set_duomice.m
+++ set_duomice.m
@@ -2,20 +2,24 @@
 !xinput create-master L
 !xinput  list
 
-% dell
+%% dell
 !xinput reattach 19 20
 
 !xinput  list
 
-% BenQ
-!xinput reattach 12 14
+%% BenQ
+
 !xinput reattach 13 14
 !xinput reattach 13 2
 !xinput reattach 11 14
 !xinput  list
 !xinput remove-master 15
+
 !xinput  list
-% hnlb
+!xinput create-master L
+!xinput  list
+!xinput reattach 12 14
+%% hnlb
 !xinput reattach 14 16
 !xinput reattach 29 16
 !xinput reattach 19 14
