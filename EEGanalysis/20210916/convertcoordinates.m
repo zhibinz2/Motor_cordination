f2e9204172bed2fd87efbf988205f89e1e994d24
ZHIBIN ZHOU <zhibinz2@uci.edu>
2021-10-01 13:16:00 -0700

    CommitName

--- EEGanalysis/20210916/convertcoordinates.m
+++ EEGanalysis/20210916/convertcoordinates.m
@@ -1,4 +1,15 @@
 
+load('chanlocs.mat')
+chanlocs=chanlocs(1:128); 
+
+X = [chanlocs.X];
+Y = [chanlocs.Y];
+Z = [chanlocs.Z];
+
+
+
+%%
+
 [azimuth,elevation,r] = cart2sph(X,Y,Z);
 
 [TH,PHI,R] = cart2sph(X,Y,Z);
