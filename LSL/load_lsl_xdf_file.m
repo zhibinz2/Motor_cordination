9ed1fe93f75060b471959775c0fe79d848f02125
ZHIBIN ZHOU <zhibinz2@uci.edu>
2021-10-27 15:57:16 -0700

    CommitName

--- LSL/load_lsl_xdf_file.m
+++ LSL/load_lsl_xdf_file.m
@@ -1,7 +1,7 @@
 addpath('/home/zhibin/Documents/GitHub/labstreaminglayer/LSL/liblsl-Matlab')
 
 
-addpath('/home/zhibin/Documents/GitHub/labstreaminglayer/LSL/liblsl-Matlab')
+addpath('/home/zhibin/Documents/GitHub/xdf-Matlab')
 
 
-streams = load_xdf('your_file_name.xdf')
+streams = load_xdf('sub-P001_ses-S001_task-Default_run-001_eeg.xdf')
