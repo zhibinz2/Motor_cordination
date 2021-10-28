0187ee1944564fbee9274174a84e7eaec31e00eb
ZHIBIN ZHOU <zhibinz2@uci.edu>
2021-10-27 21:44:33 -0700

    CommitName

--- LSL/load_lsl_xdf_file.m
+++ LSL/load_lsl_xdf_file.m
@@ -3,9 +3,15 @@ addpath('/home/zhibin/Documents/GitHub/labstreaminglayer/LSL/liblsl-Matlab')
 
 addpath('/home/zhibin/Documents/GitHub/xdf-Matlab')
 
+%%
+streamsFNIR = load_xdf('sub-P001_ses-S001_task-Default_run-001_eeg.xdf')
 
-streams = load_xdf('sub-P001_ses-S001_task-Default_run-001_eeg.xdf')
+stream1=streamsFNIR{1};% markers sent
+stream2=streamsFNIR{2};% oxysoft markers
+stream3=streamsFNIR{3};% oxysoft fnir time series 36*788
 
-stream1=streams{1};% markers sent
-stream2=streams{2};% oxysoft markers
-stream3=streams{3};% oxysoft fnir time series 36*788
+%%
+streamsABM = load_xdf('sub-P001_ses-S001_task-Default_run-001_eeg.xdf')
+
+stream1=streamsABM{1};% markers sent
+stream2=streamsABM{2};% oxysoft markers
