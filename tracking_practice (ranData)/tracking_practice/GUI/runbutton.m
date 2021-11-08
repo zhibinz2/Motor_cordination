ae972f933dde51c092447c248d975b9a4d8058c9
ZHIBIN ZHOU <zhibinz2@uci.edu>
2021-11-08 14:10:03 -0800

    CommitName

--- tracking_practice (ranData)/tracking_practice/GUI/runbutton.m
+++
@@ -1,9 +0,0 @@
-function runbutton(obj,eventdata,h)
-   
-    set(h.hEdt1,'Enable','inactive');
-    set(h.hEdt2,'Enable','inactive');
-    set(h.hEdt3,'Enable','inactive');
-    set(h.hPB,'Enable','off');
-    set(h.hText,'String','Please wait');
-
-end
--- tracking_practice (ranData)/tracking_practice/calibration.m
+++
@@ -1,6 +0,0 @@
-% Calibration.m
-addpath analysis
-
-load('calibration.mat');
-obj = audioplayer(y,fs);
-play(obj)
