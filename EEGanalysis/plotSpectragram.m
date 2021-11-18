07110027300af4f6cf064f6328cca2d6b9add665
ZHIBIN ZHOU <zhibinz2@uci.edu>
2021-11-18 00:48:53 -0800

    CommitName

--- EEGanalysis/plotSpectragram.m
+++ EEGanalysis/plotSpectragram.m
@@ -35,7 +35,8 @@ end
 baselinecorrected_data_trial;
 
 %% wavelet
-wfreq = 1:50; % frequency(s) of interest
+% wfreq = 1:50; % frequency(s) of interest
+wfreq = [2 4 6 8 10 14 18 24 30 40];
 wfc = 1.5;
 sr=1000;
 
@@ -43,55 +44,76 @@ UniCondi=unique(CondiData);
 
 % plot out pow the 128 channels
 AllchanNames={'FP1','FPZ','FP2','AF3','AF4','F11','F7','F5','F3','F1','FZ','F2','F4','F6','F8','F12','FT11','FC5','FC3','FC1','FCZ','FC2','FC4','FC6','FT12','T7','C5','C3','C1','CZ','C2','C4','C6','T8','TP7','CP5','CP3','CP1','CPZ','CP2','CP4','CP6','TP8','M1','M2','P7','P5','P3','P1','PZ','P2','P4','P6','P8','PO7','PO3','POZ','PO4','PO8','O1','OZ','O2','CB1','CB2','AFP1','AFP2','AF7','AF5','AFZ','AF6','AF8','AFF5H','AFF3H','AFF1H','AFF2H','AFF4H','AFF6H','F9','F10','FFT7H','FFC5H','FFC3H','FFC1H','FFC2H','FFC4H','FFC6H','FFT8H','FT9','FT7','FT8','FT10','FTT7H','FCC5H','FCC3H','FCC1H','FCC2H','FCC4H','FCC6H','FTT8H','TTP7H','CCP5H','CCP3H','CCP1H','CCP2H','CCP4H','CCP6H','TTP8H','TPP7H','CPP5H','CPP3H','CPP1H','CPP2H','CPP4H','CPP6H','TPP8H','P9','P10','PPO3H','PPO1H','PPO2H','PPO4H','PO9','PO5','PO1','PO2','PO6','PO10','CBZ','VEOG','HEOG','EMG1','EMG2','HL 1','HL 2','EMG3','EMG4','EMG5','EMG6','TRIGGER'};
+
 % AllchanNames{1}
 
+     
 for u=1:length(UniCondi);
 
-    figure('units','normalized','outerposition',[0 0 0.3 0.8]);
+    figure('units','normalized','outerposition',[0 0 0.8 0.8]);
 
     indtemp=find(CondiData==UniCondi(u));
     HighInd=indtemp(find(TrialScores(indtemp)>0.8));
 
-    basedlinecorrected_ERP=mean(baselinecorrected_data_trial(1501:2500,1:128,HighInd),3);
+    % Compute ERP in all channels
+    basedlinecorrected_ERP=mean(baselinecorrected_data_trial(:,1:128,HighInd),3);
     % plot(basedlinecorrected_ERP);
 
     cnorm = wavelet(basedlinecorrected_ERP,sr,wfc,wfreq);
-
+    Powcnorm = abs(cnorm).^2;
+    
+    % Power normalization
+    % one way to it:
+    logPowcorm=log(Powcnorm);
+    baselineMean=mean(logPowcorm(:,501:1000,:),2);
+    normPowcnorm = logPowcorm-(ones(1,3000,1).*baselineMean);
+    % second way to do it
+%     baselineMean = mean(Powcnorm(:,501:1000,:),2);
+% 	normPowcnorm = 100.*(Powcnorm-ones(1,3000,1).*baselineMean)./(ones(1,3000,1).*baselineMean);
+    
+    
+    % open sgolay; open sgolayfilt
 
     subplot(3,2,1);
-    imagesc(abs(cnorm(:,:,9))); % F3/F4 9/13  
-    colormap jet; colorbar ; %caxis% ([0 0.15]);% caxis 
+    imagesc((sgolayfilt(squeeze(normPowcnorm(:,:,9))',1,31))'); % F3/F4 9/13  
+    colormap jet; colorbar ; %caxis ([0 60]);% caxis 
     xlabel('time(ms)'); ylabel('frequencies(Hz)'); set(gca,'ydir','normal');
+    yticks([1:length(wfreq)]);yticklabels({'2','4','6','8','10','14','18','24','30','40'});
     title([AllchanNames{9}]);
-
+    
     subplot(3,2,2);
-    imagesc(abs(cnorm(:,:,13))); % F3/F4 9/13  
-    colormap jet; colorbar ; %caxis% ([0 0.15]);
+    imagesc((sgolayfilt(squeeze(normPowcnorm(:,:,13))',1,31))'); % F3/F4 9/13  
+    colormap jet; colorbar ; %caxis ([0 60]);
     xlabel('time(ms)'); ylabel('frequencies(Hz)'); set(gca,'ydir','normal');
+    yticks([1:length(wfreq)]);yticklabels({'2','4','6','8','10','14','18','24','30','40'});
     title([AllchanNames{13}]);
 
     subplot(3,2,3);
-    imagesc(abs(cnorm(:,:,19))); % FC3/FC4 19/23 
-    colormap jet; colorbar ; %caxis% ([0 0.15]); % caxis
+    imagesc((sgolayfilt(squeeze(normPowcnorm(:,:,19))',1,31))'); % FC3/FC4 19/23 
+    colormap jet; colorbar ; %caxis ([0 60]);
     xlabel('time(ms)'); ylabel('frequencies(Hz)'); set(gca,'ydir','normal');
+    yticks([1:length(wfreq)]);yticklabels({'2','4','6','8','10','14','18','24','30','40'});
     title([AllchanNames{19}]);
 
     subplot(3,2,4);
-    imagesc(abs(cnorm(:,:,23))); % FC3/FC4 19/23 
-    colormap jet; colorbar ; %caxis% ([0 0.15]);
+    imagesc((sgolayfilt(squeeze(normPowcnorm(:,:,23))',1,31))'); % FC3/FC4 19/23 
+    colormap jet; colorbar ; %caxis ([0 60]);
     xlabel('time(ms)'); ylabel('frequencies(Hz)'); set(gca,'ydir','normal');
+    yticks([1:length(wfreq)]);yticklabels({'2','4','6','8','10','14','18','24','30','40'});
     title([AllchanNames{23}]);
 
     subplot(3,2,5);
-    imagesc(abs(cnorm(:,:,28))); % C3 
-    colormap jet; colorbar ; %caxis% ([0 0.15]);
+    imagesc((sgolayfilt(squeeze(normPowcnorm(:,:,28))',1,31))'); % C3 
+    colormap jet; colorbar ; %caxis ([0 60]);
     xlabel('time(ms)'); ylabel('frequencies(Hz)'); set(gca,'ydir','normal');
+    yticks([1:length(wfreq)]);yticklabels({'2','4','6','8','10','14','18','24','30','40'});
     title([AllchanNames{28}]);
 
     subplot(3,2,6);
-    imagesc(abs(cnorm(:,:,32))); % C4 
-    colormap jet; colorbar ; %caxis% ([0 0.15]);
+    imagesc((sgolayfilt(squeeze(normPowcnorm(:,:,32))',1,31))'); % C4 
+    colormap jet; colorbar ; %caxis ([0 60]);
     xlabel('time(ms)'); ylabel('frequencies(Hz)'); set(gca,'ydir','normal');
+    yticks([1:length(wfreq)]);yticklabels({'2','4','6','8','10','14','18','24','30','40'});
     title([AllchanNames{32}]);
 
     suptitle([conditionNames{UniCondi(u)}]);
@@ -104,7 +126,7 @@ wfc = 1.5;
 sr=1000;
 cnorm = wavelet(filtered_data(1001:5000,:),sr,wfc,wfreq);
 
-imagesc(cnorm);
+% imagesc(cnorm);
 
 figure;imagesc(abs(cnorm))
 colormap jet
--- PerformanceAnaylisis/plot_standard_movements20211018.m
+++ PerformanceAnaylisis/plot_standard_movements20211018.m
@@ -1,7 +1,8 @@
-conditions = [0:1:12]*(pi/2/6);% 13 conditions from [0-180] degrees
+% conditions = [0:1:12]*(pi/2/6);% 13 conditions from [0-180] degrees
 
-conditions
 
+conditions = [0 2*atan(1/4) 2*atan(1/2) 2*atan(1) 2*atan(2/1) 2*atan(4/1) pi];
+conditionNames={'0:4' '1:4' '1:2' '1:1' '2:1' '4:1' '4:0'};  % conditionNames{1}
 
 % pick a condition from randomized set allPerm
 % conditionSelected = allPerm(numTrials*(block-1)+t);
@@ -12,15 +13,17 @@ for conditionSelected=1:length(conditions)
     % produced the position parameters
         rad_ang=conditions(conditionSelected);
         [x,y] = drawReach(radius,rad_ang, xCenter, yCenter);
-    plot(x,y,'k');
+    plot(x,y,'linewidth',3,'color',[rand(1,3)]);
     hold on;
 end
 hold off;
 ylim([0 screenYpixels]);xlim([0 screenXpixels]);set(gca, 'YDir', 'reverse');
+xlabel('Screen-x-pixel');ylabel('Screen-y-pixel');
+legend(conditionNames);
 
 
 %% plot out all correct movements of standard
-figure; es=2.5;
+figure; es=2.25;
 
 %     % another alternative calculation (tangent calculation)
 %     XL=xCenter-xML; XR=xMR-xCenter;
@@ -48,17 +51,21 @@ for conditionSelected=1:length(conditions)
     Rtotal=radius*cos(theta)*es;
 %     Rtotal=(x(end)/es-xCenter);
 
-    subplot(7,13,abs(7-conditionSelected)*13+(14-conditionSelected));
-    plot(x-xCenter,yCenter-y,'k');hold on;
+%     subplot(7,13,abs(7-conditionSelected)*13+(14-conditionSelected));
+%     subplot(4,7,abs(4-conditionSelected)*7+(7+1-conditionSelected));
+    subplot(1,7,7+1-conditionSelected);
+    plot(x-xCenter,yCenter-y,'g','linewidth',2);hold on;
+    xlabel('Screen-x-pixel');ylabel('Screen-y-pixel');
+    title([conditionNames{conditionSelected}]);
     if conditionSelected==1:6;
-        plot([-Ltotal 0],[0 0],'r');
+        plot([-Ltotal 0],[0 0],'r','linewidth',2);
     else
-        plot([-Ltotal 0],[0 0],'r');
+        plot([-Ltotal 0],[0 0],'r','linewidth',2);
     end
     if conditionSelected==1:6;
-        plot([-Rtotal 0], [0 0],'b');
+        plot([-Rtotal 0], [0 0],'b','linewidth',2);
     else
-        plot([Rtotal 0], [0 0],'b');
+        plot([Rtotal 0], [0 0],'b','linewidth',2);
     end
     plot(0,0,'go');hold off;
     ylim([-screenYpixels/2 screenYpixels/2]);xlim([-screenXpixels/2 screenXpixels/2]); 
