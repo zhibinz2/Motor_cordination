26348ce0a256a4d95892882d8e16a997ae5fa86b
ZHIBIN ZHOU <zhibinz2@uci.edu>
2021-12-10 11:28:51 -0800

    CommitName

--- EEGanalysis/20211102/tryout_fastica.m
+++ EEGanalysis/20211102/tryout_fastica.m
@@ -46,8 +46,18 @@ plotx(EEGdataRestore(:,10000:20000));
 
 imagesc(A);
 topoplot(A(1,:),EEG.chanlocs(1:128),'conv','on');
-topoplot(A(:,1),EEG.chanlocs(1:128),'conv','on');
+topoplot(A(:,1),test,'conv','on','nosedir','+Y');
+%%
+for i=1:12
+    subplot(1,12,i);
+    topoplot(A(:,i),test,'nosedir','+Y'); % plot each colum
+end
 
+for i=1:12
+    subplot(1,12,i);
+    topoplot(A(i,:),test,'nosedir','+Y');
+end
+%%
 imagesc(W);
 topoplot(W(1,:),EEG.chanlocs(1:128),'conv','on');
 topoplot(W(:,1),EEG.chanlocs(1:128),'conv','on');
@@ -58,9 +68,43 @@ topoplot(A(:,1),EEG.chanlocs(1:128),'conv','on');
 
 Chanlocs_topolot=EEG.chanlocs(1:128);
 
+test=EEG.chanlocs;
+
 topoplot(A(1,:),Neuroscan_topoplot_chans);
 
 topoplot(A(1,:),chanlocs(1:128),'conv','on');
+
+%% Remove #1 component
+mixedsig
+
+for i=1:128
+    SqA(i)=sumsqr(A(:,i));
+end
+plot(1:128,SqA,'ro');ylabel('sum of square of column in A');
+
+for i=1:128
+    SqA(i)=sumsqr(W(:,i));
+end
+plot(1:128,SqA,'ro');ylabel('sum of square of column in A');
+
+for i=1:128
+    SqA(i)=sumsqr(A(i,:));
+end
+plot(1:128,SqA,'ro');ylabel('sum of square of column in A');
+
+RemoveICA1sig=A(:,2:128)*icasig(2:128,:);%remove #1 component and get back 128 channel signal
+
+[icasig2, A2, W2] = fastica(RemoveICA1sig);
+
+for i=1:12
+    subplot(1,12,i);
+    topoplot(A2(:,i),test,'nosedir','+Y'); % plot each colum
+end
+
+plotx(icasig2(1:127,1000:5000)');
+
+
+
 %%
 % 139 channel labels
 chanlocs = struct('labels', {'FP1' 'FPZ' 'FP2' 'AF3' 'AF4' 'F11' 'F7' 'F5' 'F3' 'F1' 'FZ' 'F2' 'F4' 'F6' 'F8' 'F12' 'FT11' 'FC5' 'FC3' 'FC1' 'FCZ' 'FC2' 'FC4' 'FC6' 'FT12' 'T7' 'C5' 'C3' 'C1' 'CZ' 'C2' 'C4' 'C6' 'T8' 'TP7' 'CP5' 'CP3' 'CP1' 'CPZ' 'CP2' 'CP4' 'CP6' 'TP8' 'M1' 'M2' 'P7' 'P5' 'P3' 'P1' 'PZ' 'P2' 'P4' 'P6' 'P8' 'PO7' 'PO3' 'POZ' 'PO4' 'PO8' 'O1' 'OZ' 'O2' 'CB1' 'CB2' 'AFP1' 'AFP2' 'AF7' 'AF5' 'AFZ' 'AF6' 'AF8' 'AFF5H' 'AFF3H' 'AFF1H' 'AFF2H' 'AFF4H' 'AFF6H' 'F9' 'F10' 'FFT7H' 'FFC5H' 'FFC3H' 'FFC1H' 'FFC2H' 'FFC4H' 'FFC6H' 'FFT8H' 'FT9' 'FT7' 'FT8' 'FT10' 'FTT7H' 'FCC5H' 'FCC3H' 'FCC1H' 'FCC2H' 'FCC4H' 'FCC6H' 'FTT8H' 'TTP7H' 'CCP5H' 'CCP3H' 'CCP1H' 'CCP2H' 'CCP4H' 'CCP6H' 'TTP8H' 'TPP7H' 'CPP5H' 'CPP3H' 'CPP1H' 'CPP2H' 'CPP4H' 'CPP6H' 'TPP8H' 'P9' 'P10' 'PPO3H' 'PPO1H' 'PPO2H' 'PPO4H' 'PO9' 'PO5' 'PO1' 'PO2' 'PO6' 'PO10' 'CBZ' 'VEOG' 'HEOG' 'EMG1' 'EMG2' 'HL 1' 'HL 2' 'EMG3' 'EMG4' 'EMG5' 'EMG6' 'TRIGGER'});
