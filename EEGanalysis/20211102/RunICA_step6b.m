68af959b034d7246776b48040a447af4a77b2d90
ZHIBIN ZHOU <zhibinz2@uci.edu>
2021-12-13 00:41:05 -0800

    CommitName

--- EEGanalysis/20211102/RunICA_step6b.m
+++ EEGanalysis/20211102/RunICA_step6b.m
@@ -15,3 +15,18 @@ split_reRef_data = num2cell(reRef_data, [1 2]); %split A keeping dimension 1 and
 EEGdataShort=vertcat(split_reRef_data{:});
 
 %% Recalculate the event array (for display and for EEGLAB)
+% So the 4000 timepoints are structured as 
+% "500ms padding + 500ms green dot baseline + 500ms plan phase + 1000ms movement + 1000ms show bonus + 500ms padding"
+
+NumTrials=size(reRef_data,3); % NumTrials=350;
+
+% Index for start of trials
+% IndEvents=round(linspace(1000,4000*NumTrials,NumTrials));
+IndEvents=[1000:4000:4000*NumTrials]
+IndEnds=IndEvents+1500;
+
+% Index for EEGLAB
+IndEventsEEGLAB=zeros(1,size(reRef_data,1));
+IndEventsEEGLAB(IndEvents)=1;
+IndEventsEEGLAB(IndEnds)=2;
+
