8b749dc7924f95aa156faf6a375b484636d07def
ZHIBIN ZHOU <zhibinz2@uci.edu>
2021-12-12 22:52:00 -0800

    CommitName

--- EEGanalysis/20211102/RunICA_step6b.m
+++ EEGanalysis/20211102/RunICA_step6b.m
@@ -1,2 +1,17 @@
 % Run ICA on reRef_data
 % this should be step 6b
+
+%% Recombine all trials into one big long time series
+
+% % Slow Method
+% EEGdataShort=reRef_data(:,:,1);
+% for i=2:size(reRef_data,3)
+%     %EEGdataShortTemp=reRef_data(:,:,i);
+%     EEGdataShort=cat(1,EEGdataShort,reRef_data(:,:,i));
+% end
+
+% Fast Method
+split_reRef_data = num2cell(reRef_data, [1 2]); %split A keeping dimension 1 and 2 intact
+EEGdataShort=vertcat(split_reRef_data{:});
+
+%% Recalculate the events
