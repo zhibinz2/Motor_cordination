eb920b845a9c1c67b0939970fa20b9fce398387b
ZHIBIN ZHOU <zhibinz2@uci.edu>
2022-01-10 16:25:01 -0800

    CommitName

--- EEGanalysis/20211102/integrate_EEG_into_data_trials_step3.m
+++ EEGanalysis/20211102/integrate_EEG_into_data_trials_step3.m
@@ -21,21 +21,28 @@ data_trials=zeros(500+NumTrialtimepoints,NumAllChannels,NumTrialsRecorded);% add
 % trialstart=zeros(1,NumTrialsRecorded);
 trialend=zeros(1,NumTrialsRecorded);
 
-% timing (trial length) might mess up
+% Method 1 
 for ntr=1:NumTrialsRecorded % take too much memory, matlab might quit
-    % trialstart(ntr)=find(datatimes==locs(ind_trial_start(ntr)));
-    trialend(ntr)  =find(datatimes==locs(ind_trial_end(ntr)));
-    % data_trials(1:(trialend(ntr)-trialstart(ntr)+1),1:NumEEGChannels,ntr)=filtered_data(trialstart(ntr):trialend(ntr),1:NumEEGChannels); 
+    trialend(ntr)  =find(datatimes==locs(locs_ind_trial_end(ntr)));
     data_trials((1:(500+NumTrialtimepoints)),1:NumEEGChannels,ntr)=filtered_data((trialend(ntr)-(500+NumTrialtimepoints)+1):trialend(ntr),1:NumEEGChannels); 
+end
+% Keep in mind that after the filter, the last 10 seconds or so of the data is not usable)
+
+% Method 2 -- timing(trial length) might mess up
+for ntr=1:NumTrialsRecorded % take too much memory, matlab might quit
+    trialstart(ntr)=find(datatimes==locs(locs_ind_trial_start(ntr)));
+    trialend(ntr)  =find(datatimes==locs(locs_ind_trial_end(ntr)));
+    data_trials(1:(trialend(ntr)-trialstart(ntr)+1),1:NumEEGChannels,ntr)=filtered_data(trialstart(ntr):trialend(ntr),1:NumEEGChannels); 
 end
 % Keep in mind that after the filter, the last 10 seconds or so of the data is not usable)
 
 
 % examine 
-% plot(ind_trial_end-ind_trial_start,'ro');ylabel('trial peaks');xlabel('trial');
-% locsstart=locs(ind_trial_start);
-% locsend=locs(ind_trial_end);
-% plot(trialend-trialstart,'ro');ylabel('trial length');xlabel('trial');
-% plot(trialstart,ones(1,length(trialstart)),'go');hold on;plot(trialend,ones(1,length(trialend)),'ro'); % check 1 second inter-trial interval
-% plot(data_trials(:,1:128,5));
-% plot(datatimes);
+plot(ind_trial_end-ind_trial_start,'ro');ylabel('trial peaks');xlabel('trial');
+
+locsstart=locs(ind_trial_start);
+locsend=locs(ind_trial_end);
+plot(trialend-trialstart,'ro');ylabel('trial length');xlabel('trial');
+plot(trialstart,ones(1,length(trialstart)),'go');hold on;plot(trialend,ones(1,length(trialend)),'ro'); % check 1 second inter-trial interval
+plot(data_trials(:,1:128,5));
+plot(datatimes);
