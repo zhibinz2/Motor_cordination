56a841713bd21ceaccb34f459d17bba9c059f40c
Zhibin Zhou <zhibinz2@uci.edu>
2022-02-16 15:35:05 -0800

    CommitName

---
+++ fNIR_test/xdfchannels.m
@@ -0,0 +1,10 @@
+cd D:\360MoveData\Users\alienware\Documents\GitHub\Motor_cordination
+
+allPerm
+
+O2HbChans=1:2:43;
+O2HbChansL=1:2:21;
+O2HbChansR=2:2:22;
+HHbChans=2:2:44;
+HHbChansL=23:2:43;
+HHbChansR=24:2:44;
