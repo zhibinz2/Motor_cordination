ffcaeac2d686b0ed29229f2d90eaf10a87ac77ed
ZHIBIN ZHOU <zhibinz2@uci.edu>
2022-04-02 11:17:57 -0700

    CommitName

--- 1_over_f/data_analysis/hilbert_transform/hilberttryout.m
+++ 1_over_f/data_analysis/hilbert_transform/hilberttryout.m
@@ -1,4 +1,5 @@
 %% https://www.mathworks.com/help/signal/ug/hilbert-transform.html
+clear; close all;
 t = 0:1/1024:1;
 x = sin(2*pi*60*t);
 figure;
@@ -12,7 +13,17 @@ plot(t(1:50),imag(y(1:50)))
 hold off
 axis([0 0.05 -1.1 2])
 legend('Real Part','Imaginary Part')
+
+
+%******
+y2=hilbert(x);
+plot(t,y);
+hold on;
+plot(t,real(y),'g');
+plot(t,y2,'m');
 %% https://www.mathworks.com/help/signal/ug/envelope-extraction-using-the-analytic-signal.html
+clear;
+
 t = 0:1e-4:0.1;
 x = (1+cos(2*pi*50*t)).*cos(2*pi*1000*t);
 
@@ -32,5 +43,55 @@ plot(t,[-1;1]*env,plot_param{:})
 hold off
 xlim([0 0.04])
 title('Hilbert Envelope')
+
+%****************
+clear;
+
+fl1 = 12;
+[up1,lo1] = envelope(x,fl1,'analytic');
+fl2 = 30;
+[up2,lo2] = envelope(x,fl2,'analytic');
+param_small = {'Color',[0.9 0.4 0.1],'Linewidth',2};
+param_large = {'Color',[0 0.4 0],'Linewidth',2};
+
+plot(t,x)
+hold on
+p1 = plot(t,up1,param_small{:});
+plot(t,lo1,param_small{:});
+p2 = plot(t,up2,param_large{:});
+plot(t,lo2,param_large{:});
+hold off
+
+legend([p1 p2],'fl = 12','fl = 30')
+xlim([0 0.04])
+title('Analytic Envelope')
+
+%******
+wl1 = 3;
+[up1,lo1] = envelope(x,wl1,'rms');
+wl2 = 5;
+[up2,lo2] = envelope(x,wl2,'rms');
+wl3 = 300;
+[up3,lo3] = envelope(x,wl3,'rms');
+
+plot(t,x)
+hold on
+p1 = plot(t,up1,param_small{:});
+plot(t,lo1,param_small{:});
+p2 = plot(t,up2,plot_param{:});
+plot(t,lo2,plot_param{:});
+p3 = plot(t,up3,param_large{:});
+plot(t,lo3,param_large{:})
+hold off
+
+legend([p1 p2 p3],'wl = 3','wl = 5','wl = 300')
+xlim([0 0.04])
+title('RMS Envelope')
+
+
+
+%% https://www.mathworks.com/help/signal/ref/envelope.html
+
+
 
 
