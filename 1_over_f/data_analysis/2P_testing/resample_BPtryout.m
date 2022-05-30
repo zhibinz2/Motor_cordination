97967996227d21ace0e7db855225405c8112eea0
ZHIBIN ZHOU <zhibinz2@uci.edu>
2022-05-29 17:44:54 -0700

    CommitName

--- 1_over_f/data_analysis/2P_testing/resample_BPtryout.m
+++ 1_over_f/data_analysis/2P_testing/resample_BPtryout.m
@@ -6,6 +6,7 @@ BPCondi1L;BPCondi1R;
 % select the time series
 BP01=BPCondi1L;
 % convert to intervals
+cd /home/zhibin/Documents/GitHub/Motor_cordination/1_over_f/data_analysis/2P_testing
 BPintervals=Calinterval(BP01');
 AddshowframesCum=cumsum(BPintervals);
 
@@ -19,15 +20,17 @@ legend('BP intervals','Cumsum');
 % resample
 sr=unique([srL srR]) % TMSi SAGA recording sampling rate in Hz
 
-srShowframe=sr*ifi*waitframes % samples per screen flip rate interval
-sr/srShowframe % how many Psychtoolbox flips per second
-DownSampleRate=sr/(sr/srShowframe);
+% srShowframe=sr*ifi*waitframes % samples per screen flip rate interval
+% sr/srShowframe % how many Psychtoolbox flips per second
+% DownSampleRate=sr/(sr/srShowframe)
 
 open /home/zhibin/Documents/GitHub/Motor_cordination/1_over_f/data_analysis/2P_testing/timeing_2P_check.m
 open /home/zhibin/Documents/GitHub/Motor_cordination/1_over_f/exp_uni_2p/5_conditions/TestFliping_time.m
+
 FlipsFreq = (1/ifi)/waitframes % how many pyschotoolbox flip per second (feedback/photocell freq)
 DownSampleRate = sr/FlipsFreq 
 
+% Downsample
 AddshowframesDown=round(AddshowframesCum/DownSampleRate);
 % Addshowframes=round(Addshowframes/(sr/(1/ifi))/waitframes); % Downsampling rate at 40
 
@@ -36,12 +39,11 @@ figure;
 plot(AddshowframesCum,'mo'); % before downsampling 
 hold on;
 plot(AddshowframesDown,'k+'); % downsampled frames for psychtoolbox
-holf off;
+hold off;
 xlim([0 60]);title('before downsampling & after downsampling');
 legend('before downsampling', 'after downsampling');
 
-
-
+% Aligned to the 30th tap
 AddshowframesDown(1) 
 NumFramesInterval2Hz*29+1 % num of farmes at the 30th tap
 shift=AddshowframesDown(1)-(NumFramesInterval2Hz*29+1)
@@ -68,23 +70,32 @@ xlim([0 60]);
 title('before slowing & after slowing');
 legend('before slowing', 'after slowing');
 
-%% Randomization of the pace stimuli in each condition ****************************************
-    % Mean stimulus interval for 2Hz pacing
-    MeanTapInterval2Hz=0.5; % second
-    NumFramesInterval2Hz=round(MeanTapInterval2Hz/(ifi*waitframes));  
-    
-    % condition 1-4 (paced the frist 30 taps, set the rest of the frames with value zeros)
-    Showframes1=[1:NumFramesInterval2Hz:NumFramesInterval2Hz*30 zeros(1,570)]; 
-    Showframes2=Showframes1;
-    Showframes3=Showframes1;
-    Showframes4=Showframes1;
-    
-    % Mean stimulus interval for 3Hz pacing
-    MeanTapInterval3Hz=1/3; % second
-    NumFramesInterval3Hz=round(MeanTapInterval3Hz/(ifi*waitframes));  % on average 72 frames per stimulus 
-    % condition 5
-    Showframes5=[1:NumFramesInterval3Hz:NumFramesInterval3Hz*30 zeros(1,570)]; 
-    Showframes6=Showframes5;
-    
-    % combine all 6 conditions
-    Showframes=[Showframes1;Showframes2;Showframes3;Showframes4;Showframes5;Showframes6];
+%% call the above in a function resamBP
+sr=unique([srL srR]);sr=2000;
+cd /ssd/zhibin/1overf/20220515_2P
+load('20220515.mat')
+ifi;waitframes;
+cd /home/zhibin/Documents/GitHub/Motor_cordination/1_over_f/exp_uni_2p/Syncopation_2P
+[SyncoSlowShowframes] = resamBP(BP01,ifi,sr,waitframes);
+
+
+%% Randomization of the pacer stimuli in each condition ****************************************
+%     % Mean stimulus interval for 2Hz pacing
+%     MeanTapInterval2Hz=0.5; % second
+%     NumFramesInterval2Hz=round(MeanTapInterval2Hz/(ifi*waitframes));  
+%     
+%     % condition 1-4 (paced the frist 30 taps, set the rest of the frames with value zeros)
+%     Showframes1=[1:NumFramesInterval2Hz:NumFramesInterval2Hz*30 zeros(1,570)]; 
+%     Showframes2=Showframes1;
+%     Showframes3=Showframes1;
+%     Showframes4=Showframes1;
+%     
+%     % Mean stimulus interval for 3Hz pacing
+%     MeanTapInterval3Hz=1/3; % second
+%     NumFramesInterval3Hz=round(MeanTapInterval3Hz/(ifi*waitframes));  % on average 72 frames per stimulus 
+%     % condition 5
+%     Showframes5=[1:NumFramesInterval3Hz:NumFramesInterval3Hz*30 zeros(1,570)]; 
+%     Showframes6=Showframes5;
+%     
+%     % combine all 6 conditions
+%     Showframes=[Showframes1;Showframes2;Showframes3;Showframes4;Showframes5;Showframes6];
---
+++ 1_over_f/exp_uni_2p/Syncopation_2P/IntervalResting.m
@@ -0,0 +1,88 @@
+% time for resting EEG (EO=eye open; EC= eye close)
+TimeRestingEEG=30; % 30 seconds @@@@@@@@@@@@@@@@@@@@@@@@@@@@2 
+numFramesRestEye=round (TimeRestingEEG/(ifi*waitframes)); 
+
+%*************************************************************************
+% Start taking eye open resting stage EEG
+instructionStart=['Rest with eyes open for ' num2str(TimeRestingEEG/60) ' min'];
+DrawFormattedText2(instructionStart,'win',windowPtr,...
+    'sx', 'center','sy', 'center','xalign','center','yalign','top','baseColor',white);
+DrawFormattedText2(instructionStart,'win',windowPtr,...
+    'sx', xCenterL,'sy', 'center','xalign','center','yalign','top','baseColor',white);
+DrawFormattedText2(instructionStart,'win',windowPtr,...
+    'sx', xCenterR,'sy', 'center','xalign','center','yalign','top','baseColor',white);
+Screen('TextSize',windowPtr, 10); % reset font size
+instructionStart=['Experimenter hit a key to continue.'];
+DrawFormattedText2(instructionStart,'win',windowPtr,...
+    'sx', 'center','sy', screenYpixels*0.8,'xalign','center','yalign','top','baseColor',white);
+DrawFormattedText2(instructionStart,'win',windowPtr,...
+    'sx', xCenterL,'sy', screenYpixels*0.8,'xalign','center','yalign','top','baseColor',white);
+DrawFormattedText2(instructionStart,'win',windowPtr,...
+    'sx', xCenterR,'sy', screenYpixels*0.8,'xalign','center','yalign','top','baseColor',white);
+Screen('Flip',windowPtr);
+Screen('TextSize',windowPtr, 18); % reset font size to default
+% hit a key to continue
+KbStrokeWait;
+Screen('Flip',windowPtr);% Reponse with a black screen
+pause(1); % given time to return the position of arm after key press 
+
+% Flash photocells once at the start and end of eye open period 
+% Flash photocells once at the start of eye open period ###### (upper left and bottom right)
+Screen('FillRect', windowPtr, white, RightUpperSquare);  % event type = 1200001
+Screen('FillRect', windowPtr, white, LeftUpperSquare);
+Screen('Flip',windowPtr);
+
+
+
+% get a timestamp and begin taking resting EEG ****************************
+vbl = Screen('Flip', windowPtr);
+i=1;
+while i<numFramesRestEye
+% If esc is press, break out of the while loop and close the screen
+    [keyIsDown, keysecs, keyCode] = KbCheck;
+    if keyCode(KbName('escape'))
+        Screen('CloseAll');
+        break;
+    end
+    % Show the fixation cross
+    Screen('DrawDots', windowPtr, [FixCrX;FixCrY], screenYpixels/400, white, [0 0], 2);
+    Screen('DrawDots', windowPtr, [FixCrX-screenXpixels/3;FixCrY], screenYpixels/400, white, [0 0], 2);
+    Screen('DrawDots', windowPtr, [FixCrX+screenXpixels/3;FixCrY], screenYpixels/400, white, [0 0], 2);
+    % Show the central dot
+    % Screen('DrawDots', windowPtr, [xCenter;yCenter], Thickness, white, [0 0], 2);
+    % Flip the black screen
+    vbl  = Screen('Flip', windowPtr, vbl + (waitframes -0.5) * ifi);
+    % update the while loop
+    i=i+1;
+end
+
+% Flash photocells once to mark the end of open eye resting EEG collection ###### (upper left and bottom right)
+Screen('FillRect', windowPtr, white, RightUpperSquare);  % event type = 1200001
+Screen('FillRect', windowPtr, white, LeftUpperSquare);
+Screen('Flip',windowPtr);
+
+%****************************************************************************
+Screen('TextSize',windowPtr, 18);
+instructionStart=['Ready to continue?'];
+DrawFormattedText2(instructionStart,'win',windowPtr,...
+    'sx', 'center','sy', 'center','xalign','center','yalign','top','baseColor',white);
+DrawFormattedText2(instructionStart,'win',windowPtr,...
+    'sx', xCenterL,'sy', 'center','xalign','center','yalign','top','baseColor',white);
+DrawFormattedText2(instructionStart,'win',windowPtr,...
+    'sx', xCenterR,'sy', 'center','xalign','center','yalign','top','baseColor',white);
+Screen('TextSize',windowPtr, 10);
+instructionStart=['Experimenter hit a key to continue.'];
+DrawFormattedText2(instructionStart,'win',windowPtr,...
+    'sx', 'center','sy', screenYpixels*0.8,'xalign','center','yalign','top','baseColor',white);
+DrawFormattedText2(instructionStart,'win',windowPtr,...
+    'sx', xCenterL,'sy', screenYpixels*0.8,'xalign','center','yalign','top','baseColor',white);
+DrawFormattedText2(instructionStart,'win',windowPtr,...
+    'sx', xCenterR,'sy', screenYpixels*0.8,'xalign','center','yalign','top','baseColor',white);
+Screen('TextSize',windowPtr, 18);
+Screen('Flip',windowPtr);
+% hit a key to continue
+KbStrokeWait;
+Screen('Flip',windowPtr);% Response with a black screen
+pause(1); % given time to return the position of arm after key press 
+
+%*************************************************************************
