b20ef6c5dfda21106cb1941e49876381a86a3382
Zhibin Zhou <zhibinz2@uci.edu>
2022-05-30 22:08:01 -0700

    CommitName

--- 1_over_f/data_analysis/2P_testing/P2_xcorr_plots.m
+++ 1_over_f/data_analysis/2P_testing/P2_xcorr_plots.m
@@ -15,6 +15,7 @@ for condi=1:6
     if condi==4; BP_L=BPCondi4L; FB_L=; BP_R=;FB_R=;end
     if condi==5; BP_L=BPCondi5L; FB_L=; BP_R=;FB_R=;end
     if condi==6; BP_L=BPCondi6L; FB_L=; BP_R=;FB_R=;end
+    % compute
     subplot(4,6,condi); %  autocorr(BP_L)
         autocorr(Calinterval(BP_L'),500); xlabel('lags');ylabal('autocorrelation'); 
         title(['autocorr BP_L' '\nCondi ' num2str(condi)]);
@@ -52,7 +53,7 @@ for condi=1:6
     if condi==4; BP_L=BPCondi4L; BP_R=;end
     if condi==5; BP_L=BPCondi5L; BP_R=;end
     if condi==6; BP_L=BPCondi6L; BP_R=;end
-    subplot(2,6,condi); % Pspectra(BP_L)
+    subplot(4,6,condi); % Pspectra(BP_L)
         [fcoef1,fcoef2,cprod, freqs] = spectra(Calinterval(BP_L'),Calinterval(BP_L'),maxfreq,sr);
         plot(freqs,abs(cprod)); title('abs(cprod)');% cross spectra
         xlabel('freqs');ylabal('power'); 
@@ -62,7 +63,7 @@ for condi=1:6
         mdl=fitlm(tbl,'linear');
         plotAdded(mdl);
         
-    subplot(2,6,6+condi);  % Pspectra(BP_R) 
+    subplot(4,6,6+condi);  % Pspectra(BP_R) 
         [fcoef1,fcoef2,cprod, freqs] = spectra(Calinterval(BP_L'),Calinterval(BP_L'),maxfreq,sr);
         plot(freqs,abs(cprod)); title('abs(cprod)');% cross spectra
         xlabel('freqs');ylabal('power'); 
@@ -72,12 +73,12 @@ for condi=1:6
         mdl=fitlm(tbl,'linear');
         plotAdded(mdl);
         
-    subplot(2,6,6*2+condi);  % DFA(BP_L)
+    subplot(4,6,6*2+condi);  % DFA(BP_L)
         addpath 
         [D,Alpha1]=DFA_main(Calinterval(BP_L'));
         title('['DFA BP_L' '\nCondi ' num2str(condi)]');
         
-    subplot(2,6,6*2+condi);  % DFA(BP_R)
+    subplot(4,6,6*2+condi);  % DFA(BP_R)
         [D,Alpha1]=DFA_main(Calinterval(BP_R'));
         title('['DFA BP_R' '\nCondi ' num2str(condi)]');
       
@@ -85,8 +86,51 @@ for condi=1:6
 end
 
 %% P2 Error (short & long -range statistics)
-% autocorr (Error) + Pspectra(Error) + DFA (Error)
-% 6 conditions -subplots(3,6,i)
+% Error + autocorr (Error) + Pspectra(Error)+ DFA(Error)
+% uncoupled condition (bidiretional error) -subplots(4,10,1-2)
+% unidrectional conditions -subplots(4,10,3-4)
+% bidirectional condition (bidiretional error) -subplots(4,10,5-10)
+
+for condi=1:10
+    if condi==1; BP_L=BPCondi1L; BP_R=; time_series1=find(BP_L==1); time_series2=find(BP_R==1); end
+    if condi==2; BP_L=BPCondi2L; BP_R=; time_series1=find(BP_R==1); time_series2=find(BP_L==1); end
+    if condi==3; BP_L=BPCondi3L; BP_R=; time_series1=find(BP_L==1); time_series2=find(BP_R==1); end
+    if condi==4; BP_L=BPCondi4L; BP_R=;end
+    if condi==5; BP_L=BPCondi5L; BP_R=;end
+    if condi==6; BP_L=BPCondi6L; BP_R=;end
+    if condi==7; BP_L=BPCondi6L; BP_R=;end
+    if condi==8; BP_L=BPCondi6L; BP_R=;end
+    if condi==9; BP_L=BPCondi6L; BP_R=;end
+    if condi==10; BP_L=BPCondi6L; BP_R=;end
+    % Compute error
+    time_series1=find(BP_L==1);% plot(time_series1);
+    time_series2=find(BP_R==1);% plot(time_series2);
+    % TimeLength=min([length(time_series1) length(time_series2)]);
+    % TimeLength=max([length(time_series1) length(time_series2)]);
+    TimeLength=length(time_series1);
+    Error=[];
+    % find the time difference with the closest botton press 
+    for i=1:TimeLength % i=StartStim2 % syncopation has one press less than condition 1 and 3
+        [minValue,closetIndex]=min(abs(time_series2-time_series1(i))); % closetIndex in BottonPressTime
+        Error(i)=time_series2(closetIndex)-time_series1(i);
+    end
+
+    subplot(4,10,condi); % Error base on L (uncoupled)
+        plot(Error/2000,'r.');title('error');xlabel('taps');ylabel('timing error (s)');
+        title(['error (BP_L ' '\nCondi ' num2str(condi)]);
+        hold on;
+        tbl=table(freqs',abs(cprod'));
+        mdl=fitlm(tbl,'linear');
+        plotAdded(mdl);
+        
+    subplot(4,10,10+condi);  % Error base on R (uncoupled)
+        
+    subplot(4,10,10*2+condi);  %  Error base on L (unidirection)
+        
+    subplot(4,10,10*3+condi);  %  Error base on R (unidirection)
+        
+    suptitle('short & long -range statistics: error');
+end
 
 
 % use a sliding window and do it again
