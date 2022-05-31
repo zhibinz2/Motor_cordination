a3f0e0f1f0cfd41705f82a52c6321bd55258a6b4
Zhibin Zhou <zhibinz2@uci.edu>
2022-05-30 20:38:20 -0700

    CommitName

--- 1_over_f/data_analysis/2P_testing/P2_xcorr_tryout.m
+++ 1_over_f/data_analysis/2P_testing/P2_xcorr_tryout.m
@@ -44,7 +44,7 @@ subplot(4,1,2);plot(freqs,cprod);title('cprod');
 subplot(4,1,3);plot(freqs,abs(cprod)); title('abs(cprod)');% cross spectra
 subplot(4,1,4);plot(freqs,angle(cprod)); title('angle(cprod)');% phase difference
 
-% two channels
+% all channels
 x1=EEGCondi2L; % time_series1=BPCondi2L;
 x2=EEGCondi2R; 
 
---
+++ 1_over_f/data_analysis/2P_testing/spectra.m
@@ -0,0 +1,20 @@
+function [fcoef1,fcoef2,cprod, freqs] = spectra(x1,x2,maxfreq,sr);
+% x1 and x2 should be of the same length in the first dimension
+% This function can be used for Powerspectra or Cross spectra
+
+SamplesLength=size(x2,1);
+T=SamplesLength/sr; % in second
+df=1/T;
+nbins = ceil(maxfreq/df) + 1;
+freqs = [0:(nbins-1)]*df;
+
+fcoef1 = fft(ndetrend(x1,1),[],1)/SamplesLength;
+fcoef2 = fft(ndetrend(x2,1),[],1)/SamplesLength;
+
+cprod=[];
+for i=1:nbins
+    cprod(i,:)=fcoef1(i,:).*conj(fcoef2(i,:))/SamplesLength;
+end
+
+end
+
