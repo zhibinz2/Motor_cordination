0c717d3036f8aa77d70ab9b846da46e44b36581e
ZHIBIN ZHOU <zhibinz2@uci.edu>
2022-06-23 09:33:52 -0700

    CommitName

--- fNIR_test/cominterface_expts/Run_stimuli_tapping.m
+++ fNIR_test/cominterface_expts/Run_stimuli_tapping.m
@@ -12,6 +12,30 @@ rng(seed);
 % based on OpenGL or Screen() is not working properly.
 AssertOpenGL;
 
+%% Set trial conditions ****************************************************
+conditions = [1 2];
+conditionNames={'L' 'R'}; 
+% Instruction for the experimentor
+ConditionInstructions={'>' '<'}; 
+
+% Block & Trial number of the experiment **********************************
+% number of trials per block
+numTrials=40;
+% number of blocks
+numBlock=1;
+% total trial number
+numtotal=numTrials*numBlock; 
+% num of conditions in the experiment
+numconditions=length(conditions);
+% **********************************Block & Trial number of the experiment
+
+% % how many semirandom permutation set in the experiment 
+numPerm=numtotal/numconditions;
+% create the whole set of random conditions for the experiment
+allPerm=[];
+for p=1:numPerm
+    allPerm=[allPerm randperm(numconditions)];
+end
 
 % Set up cominterface
 oxy = actxserver('oxysoft.oxyapplication')
@@ -45,6 +69,8 @@ try      % if anything went wrong, exit the display and show the error on the Co
     white = [white white white];
     black = [black black black];
     % Initialize some other colors
+    red   = [1 0 0];
+    blue  = [0 0 1];
     green = [0 1 0];
 
     % Open an on screen window and color it black
@@ -88,33 +114,21 @@ try      % if anything went wrong, exit the display and show the error on the Co
       error('Error: Screen flash frequency is not set at 60Hz.');    
     end
 
-    %% Checkerboard Design*************************************************
-    % Green and white squares in 2x2 tiles fit into 5x5 arrangement
-    greenSquare=repmat(cat(3,0,1,0),10,10);
-    whiteSquare=repmat(cat(3,1,1,1),10,10);
-    QuadraSquare1=[greenSquare whiteSquare;whiteSquare greenSquare];
-    QuadraSquare2=[whiteSquare greenSquare;greenSquare whiteSquare];
-    K1=repmat(QuadraSquare1,5,5);
-    K2=repmat(QuadraSquare2,5,5);
-    % MakeTexture (two reversing checkerboard arrangements)
-    textureIndex1=Screen('MakeTexture',windowPtr,K1);
-    textureIndex2=Screen('MakeTexture',windowPtr,K2);
-    % Position of the checkerboard
-    pos = [xCenter-screenXpixels/4,yCenter-screenXpixels/4,xCenter+screenXpixels/4,yCenter+screenXpixels/4];
-
-    %% Set size of the squares for photocell ##############################
-    PhotosensorSize=60; 
-    % Positions of the photocell at he bottom rihgt corner of the screen
-    RightBottomSquare= [screenXpixels-PhotosensorSize screenYpixels-PhotosensorSize screenXpixels screenYpixels];
-
-    %% Timing of each video frame to flip the checkboard or the black screen
-    % Numer of frames to wait when specifying good timing. Note: the use of
-    % wait frames is to show a generalisable coding. For example, by using
-    % waitframes = 2 one would flip on every other frame. See the PTB
+    %% Numer of frames to wait when specifying good timing. 
+    % Note: the use of wait frames is to show a generalisable coding. 
+    % For example, by using waitframes = 2 one would flip on every other frame. See the PTB
     % documentation for details. In what follows we flip every frame. 
-    % In order to reverse the checkerboard at 8Hz
-    % We have to reverse the checkerboard every "(1/8 seconds)/ifi" video frames
-    waitframes = (1/8)/ifi;
+    % In order to flip with feedback at 50 Hz
+    % We have to filp every (1/50)/ifi frames or 1/50 ms/frame
+    waitframes=2;
+
+    %% Randomization of the pace stimuli in each condition ****************************************
+    % Mean stimulus interval for 1.3Hz pacing (750ms per tap)
+    MeanTapInterval13Hz=0.75; % second
+    NumFramesInterval13Hz=round(MeanTapInterval13Hz/(ifi*waitframes));  
+    
+    % 10 second (paced 13 taps)
+    Showframes=[1:NumFramesInterval13Hz:NumFramesInterval13Hz*13 zeros(1,570)]; 
 
     %% Hide the cursor of the mouse in the display*****************
     % Get handles for all virtual pointing devices, aka cursors:
@@ -128,10 +142,9 @@ try      % if anything went wrong, exit the display and show the error on the Co
     numFramesStim=round(StimulusDuration/ifi/waitframes);
 
     %% Tell subject to start
-    instructionStart=['Each stimulus will show up after a resting period of 20-30 seconds with a black screen.'...
-        '\nJust stay still and look at the screen for about 24 minutes.' ...
-        ''...
-        '\nNow. Press any key to start!']; 
+    instructionStart=['When you see X, take a rest.'...
+        '\n >Tap with left hand \nTap with right hand.'...
+        '\nPress any key to start!']; 
     DrawFormattedText2(instructionStart,'win',windowPtr,...
         'sx','center','sy','center','xalign','center','yalign','center','baseColor',white);
     Screen('Flip',windowPtr);
@@ -141,7 +154,7 @@ try      % if anything went wrong, exit the display and show the error on the Co
     Screen('Flip',windowPtr);
 
     %% initialize some variables before entering the loops of blocks
-    numBlock=40; % Do 40 stimuli
+    numBlock=1; % Do 40 stimuli
     RestDurations=[]; % Record the durations of resting intervals
     StimDurations=[]; % Record the durations of stimuli
     
@@ -154,7 +167,14 @@ try      % if anything went wrong, exit the display and show the error on the Co
         break;
         end
 
-        %% Resting with black screen before each stimulus ******************************* 
+        %******************** Within block loop
+        % how many trials 
+        for t=1:numTrials 
+        % pick a condition from randomized set allPerm
+            conditionSelected = allPerm(numTrials*(block-1)+t);
+            Showframeselected=Showframes(conditionSelected,:);
+            
+        % Resting with black screen before each stimulus ******************************* 
         % Set the time for the resting interval before/after each stimulus with randomized jittering  
         restLength = 25 + 10.*(rand(1)-0.5); % uniform distribution in seconds, with a mean of 25 s (20-30s)
         numFramesRest = round(restLength / ifi / waitframes);
@@ -183,19 +203,8 @@ try      % if anything went wrong, exit the display and show the error on the Co
         end
         % Display the duration in command window
         RestDuration=toc % check the time length in command window after experiment
-
-        % flash the photocell again to mark the end of resting
-        % Screen('FillRect', windowPtr, white, RightBottomSquare);
-        % Screen('Flip', windowPtr);
         
         %% Show the stimulus
-        % flash photocell once at the Bottom right cornor on the screen to
-        % mark the beginning of stimulus
-        % Screen('FillRect', windowPtr, white, RightBottomSquare);
-        % Screen('Flip', windowPtr);
-        % pause(0.3); 
-
-          
         % get a timestamp by flip a black screen at the start of stimulus 
         vbl = Screen('Flip', windowPtr);
 		oxy.WriteEvent('A','stim')
@@ -210,14 +219,10 @@ try      % if anything went wrong, exit the display and show the error on the Co
                 break;
             end
 
-            % Draw two reversing checkerboard pattern alternatively
-            if mod(n,2) == 1; % if n is odd number
-                Screen('DrawTexture', windowPtr, textureIndex1, [],pos);
-            end
-            if mod(n,2) == 0; % if n is even number
-                Screen('DrawTexture', windowPtr, textureIndex2, [],pos);
-            end
-
+            % Draw < or >
+            ShowCondition=[ConditionInstructions{conditionSelected}]; % Center monitor
+            DrawFormattedText2(ShowCondition,'win',windowPtr,...
+            'sx','center','sy', 'center','xalign','center','yalign','top','baseColor',color);
             % Flip to the screen
             vbl  = Screen('Flip', windowPtr, vbl + (waitframes -0.5) * ifi);
 
@@ -228,10 +233,6 @@ try      % if anything went wrong, exit the display and show the error on the Co
         oxy.WriteEvent('B','rest')
         % Display the duration in command window
         StimDuration=toc % check the time length in command window after experiment
-
-        % flash the photocell again to mark the end of resting
-        % Screen('FillRect', windowPtr, white, RightBottomSquare);
-        % Screen('Flip', windowPtr);
 
         %% Save the time durations for checking after the experiment
         RestDurations=[RestDurations;RestDuration];
