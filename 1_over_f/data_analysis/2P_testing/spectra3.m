d1b0680788a6092f260deb768b7c368e19d39b99
ZHIBIN ZHOU <zhibinz2@uci.edu>
2022-07-23 13:01:08 -0700

    CommitName

--- 1_over_f/data_analysis/2P_testing/spectra3.m
+++ 1_over_f/data_analysis/2P_testing/spectra3.m
@@ -1,13 +1,30 @@
-function [outputArg1,outputArg2] = spectra3(inputArg1,inputArg2)
+function [xx] = spectra3(x,sr,win)
 % Reorganize EEG into time x chan x chunks
 % Then fft and get power freq x chan
 % Use a window of 10s with df 0.1Hz and sum up each one Hz range
 
+%%
+% Input: x is EEG (time x chan)
+%%
+if win==[]; win=10; end % chunck of 10 seconds
+SamplesLength=sr*win; % number of samples in the window
 
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
+% maximum number of 10 second windows (# of chuncks)
+% nTrials=min([floor(length(x1)/SamplesLength) floor(length(x2)/SamplesLength)]);
+nTrials=floor(size(x,1)/SamplesLength);
 
+% zcore transform
+% x1=zscore(x1);x2=zscore(x2);
+x=zscore(x);
 
+% reorganize the time series into trials (time x chunks)
+% xx1=[];xx2=[];
+xx=[];
+for i=1:nTrials
+%     xx1(:,i)=x1((SamplesLength*(i-1)+1):(SamplesLength*i),:);
+%     xx2(:,i)=x2((SamplesLength*(i-1)+1):(SamplesLength*i),:);
+    xx(:,:,nTrials)=x((SamplesLength*(i-1)+1):(SamplesLength*i),:);
+end
 
 end
 
