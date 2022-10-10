56ba71793a6052241c8aacdfcb8128cb9d6b8388
ZHIBIN ZHOU <zhibinz2@uci.edu>
2022-10-10 10:34:05 -0700

    CommitName

---
+++ 1_over_f/data_analysis/AR/AR_tryout.m
@@ -0,0 +1,12 @@
+%% AR tryout
+
+cd /home/zhibin/Documents/GitHub/matlab/external/arfit
+open arfit
+open arconf
+
+cd /home/zhibin/Documents/GitHub/matlab/external/MVGC/core
+open tsdata_to_autocov.m % G
+open tsdata_to_var.m % A
+
+cd /home/zhibin/Documents/GitHub/matlab/external/MSE-VARFI
+open arfi2ar2.m
