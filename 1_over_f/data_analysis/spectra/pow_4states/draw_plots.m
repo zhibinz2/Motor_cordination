d628d5ecc0877986e7b81ab0d4ee59e76f81c9fe
ZHIBIN ZHOU <zhibinz2@uci.edu>
2023-01-11 09:38:20 -0800

    CommitName

--- 1_over_f/data_analysis/spectra/pow_4states/draw_plots.m
+++ 1_over_f/data_analysis/spectra/pow_4states/draw_plots.m
@@ -1,9 +1,11 @@
 figure;
 tic
-cmin=0;cmax=1e-4;
+cmin=0.25e-4;cmax=5e-4;
 for st=1:4
+    pow_mat=[];
+    pow_mat=squeeze(avg_state_pow(st,:,:));
     subplot(1,4,st);
-    imagesc(squeeze(avg_state_pow(st,:,:)));colorbar;colormap('jet');
+    imagesc(pow_mat(1:30,:));colorbar;colormap('jet');
     clim([cmin cmax]);
     xlabel('chans');ylabel('freq');set(gca, 'YDir','normal');
     title(states4names{st},'color',condicolors(st,:));
