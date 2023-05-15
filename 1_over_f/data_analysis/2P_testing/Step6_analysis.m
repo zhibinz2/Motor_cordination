dbb56cb707c7c92f0d11571943c8cb584215b691
ZHIBIN ZHOU <zhibinz2@uci.edu>
2023-05-15 12:12:08 -0700

    reproduce

--- 1_over_f/data_analysis/2P_testing/Step6_analysis.m
+++ 1_over_f/data_analysis/2P_testing/Step6_analysis.m
@@ -2759,6 +2759,199 @@ sg.Rotation=90
 set(gcf,'color','w'); % set background white for copying in ubuntu
 end
 
+%% PLOT 10-2 H matching as in Marmelat 2012 (matched int) Reproduced the plot in poster
+for subplots3 =1;
+figure; % canvas(0.5,0.35);
+subplot(1,3,1); %##############
+for subplot1 =1;
+plot(H_Lall(uncoupleInd_LR(synchind(1:3))),H_Rall(uncoupleInd_LR(synchind(1:3))),'.','MarkerSize',30,'color',darkgreen);hold on;
+plot(H_Lall(uncoupleInd_LR(synchind(4:6))),H_Rall(uncoupleInd_LR(synchind(4:6))),'square','MarkerSize',10,'color',darkgreen,'MarkerFaceColor',darkgreen);
+plot(H_Lall(uncoupleInd_LR(synchind(7:9))),H_Rall(uncoupleInd_LR(synchind(7:9))),'^','MarkerSize',8,'color',darkgreen,'MarkerFaceColor',darkgreen);
+plot(H_Lall(uncoupleInd_LR(synchind(10:12))),H_Rall(uncoupleInd_LR(synchind(10:12))),'pentagram','MarkerSize',10,'color',darkgreen,'MarkerFaceColor',darkgreen);
+plot(H_Lall(uncoupleInd_LR(synchind(13:15))),H_Rall(uncoupleInd_LR(synchind(13:15))),'*','MarkerSize',10,'color',darkgreen,'MarkerFaceColor',darkgreen);
+plot(H_Lall(uncoupleInd_LR(synchind(16:18))),H_Rall(uncoupleInd_LR(synchind(16:18))),'diamond','MarkerSize',10,'color',darkgreen,'MarkerFaceColor',darkgreen);
+plot(H_Lall(uncoupleInd_LR(syncoind(1:3))),H_Rall(uncoupleInd_LR(syncoind(1:3))),'.','MarkerSize',30,'color',pink,'MarkerFaceColor',pink);
+plot(H_Lall(uncoupleInd_LR(syncoind(4:6))),H_Rall(uncoupleInd_LR(syncoind(4:6))),'square','MarkerSize',10,'color',pink,'MarkerFaceColor',pink);
+plot(H_Lall(uncoupleInd_LR(syncoind(7:9))),H_Rall(uncoupleInd_LR(syncoind(7:9))),'^','MarkerSize',8,'color',pink,'MarkerFaceColor',pink);
+plot(H_Lall(uncoupleInd_LR(syncoind(10:12))),H_Rall(uncoupleInd_LR(syncoind(10:12))),'pentagram','MarkerSize',10,'color',pink,'MarkerFaceColor',pink);
+plot(H_Lall(uncoupleInd_LR(syncoind(13:15))),H_Rall(uncoupleInd_LR(syncoind(13:15))),'*','MarkerSize',8,'color',pink,'MarkerFaceColor',pink);
+plot(H_Lall(uncoupleInd_LR(syncoind(16:18))),H_Rall(uncoupleInd_LR(syncoind(16:18))),'diamond','MarkerSize',10,'color',pink,'MarkerFaceColor',pink);
+xlabel('DFA exponent, Participant L','FontSize',15);
+ylabel('DFA exponent, Participant R','FontSize',15);
+title('Independent','FontSize',15);
+
+% A=[];S=[]; Alpha1=[];FitValues=[];
+% A=polyfit(H_Lall(uncoupleInd_LR(1:36)),H_Rall(uncoupleInd_LR_LR(1:36)),1);
+% Alpha1=A(1); % the slope, the first order polynomial coefficient from polyfit (Hurst Componenet >1 ?)
+% FitValues=polyval(A,H_Lall(uncoupleInd_LR(1:36)));
+% hold on; plot(H_Lall(uncoupleInd_LR(1:36)),FitValues,'k-','LineWidth',3);
+
+% fit the synch data
+A=[];Alpha1=[];FitValues=[];RHO=[];xxx=[];yyy=[];
+[xxx,yyy]=keepvalues(H_Lall(uncoupleInd_LR(1:2:35)),H_Rall(uncoupleInd_LR(1:2:35)),0.2,1.4);
+A=polyfit(xxx,yyy,1);
+[RHO,~]=corr(xxx,yyy);
+Alpha1=A(1); % the slope, the first order polynomial coefficient from polyfit (Hurst Componenet >1 ?)
+FitValues=polyval(A,xxx);
+hold on; plot(xxx,FitValues,'-','color',darkgreen,'LineWidth',3);
+text(min(xxx),max(FitValues)+0.025,sprintf('\\rho=%.2f',RHO),'Color',darkgreen,'FontSize', 15)
+
+% fit the synco data
+A=[];Alpha1=[];FitValues=[];RHO=[];xxx=[];yyy=[];
+[xxx,yyy]=keepvalues(H_Lall(uncoupleInd_LR(2:2:36)),H_Rall(uncoupleInd_LR(2:2:36)),0.2,1.4);
+A=polyfit(xxx,yyy,1);
+[RHO,~]=corr(xxx,yyy);
+Alpha1=A(1); % the slope, the first order polynomial coefficient from polyfit (Hurst Componenet >1 ?)
+FitValues=polyval(A,xxx);
+hold on; plot(xxx,FitValues,'-','color',pink,'LineWidth',3);
+% xlim([0.5 1]);ylim([0.5 1]);
+xlim([0.2 1.4]);ylim([0.2 1.4]);
+plot([0.5 1], [0.5 1],'m--');
+hold off;
+text(min(xxx),min(FitValues)-0.025,sprintf('\\rho=%.2f',RHO),'Color',pink,'FontSize', 15)
+% legend('synch','','','','synco','','','','location','northwest');
+% subtitle(['(uncouple: linear regression slope = ' num2str(Alpha1,'%.3f') ')'])
+grid on;
+end
+subplot(1,3,2); %##############
+for subplot2 =1;
+plot(H_Lall(leadingInd_LR(synchind(1:3))),H_Rall(leadingInd_LR(synchind(1:3))),'.','MarkerSize',30,'color',darkgreen); hold on;
+plot(H_Lall(leadingInd_LR(synchind(4:6))),H_Rall(leadingInd_LR(synchind(4:6))),'square','MarkerSize',10,'color',darkgreen,'MarkerFaceColor',darkgreen);
+plot(H_Lall(leadingInd_LR(synchind(7:9))),H_Rall(leadingInd_LR(synchind(7:9))),'^','MarkerSize',8,'color',darkgreen,'MarkerFaceColor',darkgreen);
+plot(H_Lall(leadingInd_LR(synchind(10:12))),H_Rall(leadingInd_LR(synchind(10:12))),'pentagram','MarkerSize',10,'color',darkgreen,'MarkerFaceColor',darkgreen);
+plot(H_Lall(leadingInd_LR(synchind(13:15))),H_Rall(leadingInd_LR(synchind(13:15))),'*','MarkerSize',10,'color',darkgreen,'MarkerFaceColor',darkgreen);
+plot(H_Lall(leadingInd_LR(synchind(16:18))),H_Rall(leadingInd_LR(synchind(16:18))),'diamond','MarkerSize',10,'color',darkgreen,'MarkerFaceColor',darkgreen);
+plot(H_Lall(leadingInd_LR(syncoind(1:3))),H_Rall(leadingInd_LR(syncoind(1:3))),'.','MarkerSize',30,'color',pink);
+plot(H_Lall(leadingInd_LR(syncoind(4:6))),H_Rall(leadingInd_LR(syncoind(4:6))),'square','MarkerSize',10,'color',pink,'MarkerFaceColor',pink);
+plot(H_Lall(leadingInd_LR(syncoind(7:9))),H_Rall(leadingInd_LR(syncoind(7:9))),'^','MarkerSize',8,'color',pink,'MarkerFaceColor',pink);
+plot(H_Lall(leadingInd_LR(syncoind(10:12))),H_Rall(leadingInd_LR(syncoind(10:12))),'pentagram','MarkerSize',10,'color',pink,'MarkerFaceColor',pink);
+plot(H_Lall(leadingInd_LR(syncoind(13:15))),H_Rall(leadingInd_LR(syncoind(13:15))),'*','MarkerSize',10,'color',pink,'MarkerFaceColor',pink);
+plot(H_Lall(leadingInd_LR(syncoind(16:18))),H_Rall(leadingInd_LR(syncoind(16:18))),'diamond','MarkerSize',10,'color',pink,'MarkerFaceColor',pink);
+plot(H_Rall(followingInd_LR(synchind(1:3))),H_Lall(followingInd_LR(synchind(1:3))),'.','MarkerSize',30,'color',darkgreen);
+plot(H_Rall(followingInd_LR(synchind(4:6))),H_Lall(followingInd_LR(synchind(4:6))),'square','MarkerSize',10,'color',darkgreen,'MarkerFaceColor',darkgreen);
+plot(H_Rall(followingInd_LR(synchind(7:9))),H_Lall(followingInd_LR(synchind(7:9))),'^','MarkerSize',8,'color',darkgreen,'MarkerFaceColor',darkgreen);
+plot(H_Rall(followingInd_LR(synchind(10:12))),H_Lall(followingInd_LR(synchind(10:12))),'pentagram','MarkerSize',10,'color',darkgreen,'MarkerFaceColor',darkgreen);
+plot(H_Rall(followingInd_LR(synchind(13:15))),H_Lall(followingInd_LR(synchind(13:15))),'*','MarkerSize',10,'color',darkgreen,'MarkerFaceColor',darkgreen);
+plot(H_Rall(followingInd_LR(synchind(16:18))),H_Lall(followingInd_LR(synchind(16:18))),'diamond','MarkerSize',10,'color',darkgreen,'MarkerFaceColor',darkgreen);
+plot(H_Rall(followingInd_LR(syncoind(1:3))),H_Lall(followingInd_LR(syncoind(1:3))),'.','MarkerSize',30,'color',pink);
+plot(H_Rall(followingInd_LR(syncoind(4:6))),H_Lall(followingInd_LR(syncoind(4:6))),'square','MarkerSize',10,'color',pink,'MarkerFaceColor',pink);
+plot(H_Rall(followingInd_LR(syncoind(7:9))),H_Lall(followingInd_LR(syncoind(7:9))),'^','MarkerSize',8,'color',pink,'MarkerFaceColor',pink);
+plot(H_Rall(followingInd_LR(syncoind(10:12))),H_Lall(followingInd_LR(syncoind(10:12))),'pentagram','MarkerSize',10,'color',pink,'MarkerFaceColor',pink);
+plot(H_Rall(followingInd_LR(syncoind(13:15))),H_Lall(followingInd_LR(syncoind(13:15))),'pentagram','MarkerSize',10,'color',pink,'MarkerFaceColor',pink);
+plot(H_Rall(followingInd_LR(syncoind(16:18))),H_Lall(followingInd_LR(syncoind(16:18))),'pentagram','MarkerSize',10,'color',pink,'MarkerFaceColor',pink);
+xlabel('DFA exponent, Leader','FontSize',15);
+ylabel('DFA exponent, Follower','FontSize',15);
+title('Unidirectional','FontSize',15);
+
+% A=[];Alpha1=[];FitValues=[];
+% A=polyfit([H_Lall(leadingInd_LR(1:36)); H_Rall(followingInd_LR(1:36))], [H_Rall(leadingInd_LR(1:36)); H_Lall(followingInd_LR(1:36))],1);
+% Alpha1=A(1); % the slope, the first order polynomial coefficient from polyfit (Hurst Componenet >1 ?)
+% FitValues=polyval(A,[H_Lall(leadingInd_LR(1:36)); H_Rall(followingInd_LR(1:36))]);
+% hold on; plot([H_Lall(leadingInd_LR(1:36)); H_Rall(followingInd_LR(1:36))],FitValues,'k-','LineWidth',3);
+
+% fit the synch data
+A=[];Alpha1=[];FitValues=[];RHO=[];xxx=[];yyy=[];
+[xxx,yyy]=keepvalues([H_Lall(leadingInd_LR(1:2:35)); H_Rall(followingInd_LR(1:2:35))], ...
+    [H_Rall(leadingInd_LR(1:2:35)); H_Lall(followingInd_LR(1:2:35))],0.2,1.4);
+A=polyfit(xxx, yyy,1);
+[RHO,~]=corr(xxx, yyy);
+Alpha1=A(1); % the slope, the first order polynomial coefficient from polyfit (Hurst Componenet >1 ?)
+FitValues=polyval(A,xxx);
+hold on; 
+plot(xxx,FitValues,'-','color',darkgreen,'LineWidth',3);
+text(max(xxx)-0.1,max(FitValues)+0.02,sprintf('\\rho=%.2f',RHO),'Color',darkgreen,'FontSize', 15)
+
+% fit the synco data
+A=[];Alpha1=[];FitValues=[];RHO=[];xxx=[];yyy=[];
+[xxx,yyy]=keepvalues([H_Lall(leadingInd_LR(2:2:36)); H_Rall(followingInd_LR(2:2:36))],...
+    [H_Rall(leadingInd_LR(2:2:36)); H_Lall(followingInd_LR(2:2:36))],0.2,1.4);
+A=polyfit(xxx, yyy,1);
+[RHO,~]=corr(xxx, yyy);
+Alpha1=A(1); % the slope, the first order polynomial coefficient from polyfit (Hurst Componenet >1 ?)
+FitValues=polyval(A,xxx);
+hold on; plot(xxx,FitValues,'-','color',pink,'LineWidth',3);
+text(max(xxx)-0.1,max(FitValues)-0.03,sprintf('\\rho=%.2f',RHO),'Color',pink,'FontSize', 15)
+% xlim([0.5 1]);ylim([0.5 1]);
+xlim([0.2 1.4]);ylim([0.2 1.4]);
+plot([0.5 1], [0.5 1],'m--');
+plot([0.5 1], [0.5 1],'m--');
+hold off;
+legend('synch','','','','synco','','','','','','','','','','','','location','northwest');
+subtitle(['(leading or following: linear regression slope = ' num2str(Alpha1,'%.3f') ')'])
+grid on;
+end
+subplot(1,3,3); %##############
+for subplot3 =1;
+plot(H_Lall(mutualInd_LR(synchind(1:3))),H_Rall(mutualInd_LR(synchind(1:3))),'.','MarkerSize',30,'color',darkgreen);hold on;
+plot(H_Lall(mutualInd_LR(synchind(4:6))),H_Rall(mutualInd_LR(synchind(4:6))),'square','MarkerSize',10,'color',darkgreen,'MarkerFaceColor',darkgreen);
+plot(H_Lall(mutualInd_LR(synchind(7:9))),H_Rall(mutualInd_LR(synchind(7:9))),'^','MarkerSize',8,'color',darkgreen,'MarkerFaceColor',darkgreen);
+plot(H_Lall(mutualInd_LR(synchind(10:12))),H_Rall(mutualInd_LR(synchind(10:12))),'pentagram','MarkerSize',10,'color',darkgreen,'MarkerFaceColor',darkgreen);
+plot(H_Lall(mutualInd_LR(synchind(13:15))),H_Rall(mutualInd_LR(synchind(13:15))),'*','MarkerSize',10,'color',darkgreen,'MarkerFaceColor',darkgreen);
+plot(H_Lall(mutualInd_LR(synchind(16:18))),H_Rall(mutualInd_LR(synchind(16:18))),'diamond','MarkerSize',10,'color',darkgreen,'MarkerFaceColor',darkgreen);
+plot(H_Lall(mutualInd_LR(syncoind(1:3))),H_Rall(mutualInd_LR(syncoind(1:3))),'.','MarkerSize',30,'color',pink);
+plot(H_Lall(mutualInd_LR(syncoind(4:6))),H_Rall(mutualInd_LR(syncoind(4:6))),'square','MarkerSize',10,'color',pink,'MarkerFaceColor',pink);
+plot(H_Lall(mutualInd_LR(syncoind(7:9))),H_Rall(mutualInd_LR(syncoind(7:9))),'^','MarkerSize',8,'color',pink,'MarkerFaceColor',pink);
+plot(H_Lall(mutualInd_LR(syncoind(10:12))),H_Rall(mutualInd_LR(syncoind(10:12))),'pentagram','MarkerSize',10,'color',pink,'MarkerFaceColor',pink);
+plot(H_Lall(mutualInd_LR(syncoind(13:15))),H_Rall(mutualInd_LR(syncoind(13:15))),'*','MarkerSize',10,'color',pink,'MarkerFaceColor',pink);
+plot(H_Lall(mutualInd_LR(syncoind(16:18))),H_Rall(mutualInd_LR(syncoind(16:18))),'diamond','MarkerSize',10,'color',pink,'MarkerFaceColor',pink);
+xlabel('DFA exponent, Participant L','FontSize',15);
+ylabel('DFA exponent, Participant R','FontSize',15);
+title('Bidirectional','FontSize',15); 
+% xlim([0.5 1]);ylim([0.5 1]);
+xlim([0.2 1.4]);ylim([0.2 1.4]);
+% fit all the data
+% A=[];Alpha1=[];FitValues=[];RHO=[];xxx=[];yyy=[];
+% [xxx,yyy]=keepvalues(H_Lall(mutualInd_LR(1:36)),...
+%     H_Rall(mutualInd_LR(1:36)),0.5,1);
+% A=polyfit(xxx,yyy,1);
+% [RHO,~]=corr(xxx,yyy);
+% Alpha1=A(1); 
+% FitValues=polyval(A,xxx);
+% hold on; plot(xxx,FitValues,'k-','LineWidth',3);
+% text(min(xxx),min(FitValues)-0.025,sprintf('\\rho=%.2f',RHO),'Color',[0 0 0],'FontSize', 15)
+
+% fit the synch data
+A=[];Alpha1=[];FitValues=[];RHO=[];xxx=[];yyy=[];
+[xxx,yyy]=keepvalues(H_Lall(mutualInd_LR(1:2:35)),...
+    H_Rall(mutualInd_LR(1:2:35)),0.2,1.4);
+A=polyfit(xxx,yyy,1);
+[RHO,~]=corr(xxx,yyy);
+Alpha1=A(1); % the slope, the first order polynomial coefficient from polyfit (Hurst Componenet >1 ?)
+FitValues=polyval(A,xxx);
+hold on; plot(xxx,FitValues,'-','color',darkgreen,'LineWidth',3);
+text(min(xxx),min(FitValues)+0.07,sprintf('\\rho=%.2f',RHO),'Color',darkgreen,'FontSize', 15)
+
+% fit the synco data
+A=[];Alpha1=[];FitValues=[];RHO=[];xxx=[];yyy=[];
+[xxx,yyy]=keepvalues(H_Lall(mutualInd_LR(2:2:36)),...
+    H_Rall(mutualInd_LR(2:2:36)),0.2,1.4);
+A=polyfit(xxx,yyy,1);
+[RHO,~]=corr(xxx,yyy);
+Alpha1=A(1); % the slope, the first order polynomial coefficient from polyfit (Hurst Componenet >1 ?)
+FitValues=polyval(A,xxx);
+hold on; plot(xxx,FitValues,'-','color',pink,'LineWidth',3);
+text(max(xxx)-0.15,max(FitValues)-0.025,sprintf('\\rho=%.2f',RHO),'Color',pink,'FontSize', 15)
+% xlim([0.5 1]);ylim([0.5 1]);
+xlim([0.2 1.4]);ylim([0.2 1.4]);
+plot([0.5 1], [0.5 1],'m--');
+hold off;
+% subtitle(['(mutual): linear regression slope = ' num2str(Alpha1,'%.3f') ')'])
+grid on;
+end
+lg=legend('synch - subj pair 1','synch - subj pair 2','synch - subj pair 3','synch - subj pair 4', ...
+    'synch - subj pair 5','synch - subj pair 6', ...
+    'synco - subj pair 1','synco - subj pair 2','synco - subj pair 3','synco - subj pair 4',...
+    'synco - subj pair 5','synco - subj pair 6','location','eastoutside');
+lg.Position = [0.9475 0.15 0.01 0.75];
+% sgtitle(['H matching as in Marmelat 2012  ^{* PLOT 10-2}']);
+delete(findall(gcf,'type','annotation'))
+sg=annotation('textbox',[0.07 0.01 0.22 0.05],'string',...
+    'H matching as in Marmelat 2012  ^{* PLOT 10-2}')
+sg.Rotation=90
+set(gcf,'color','w'); % set background white for copying in ubuntu
+end
+
+
 %% PLOT 10-3 EEG matching for theta only in mutual condition
 % select 6 channels  ---  Left: 5-F3,9-FC5,14-T7; Right: 7-F4,12-FC6,18-T6
 chan6=[5 7 9 12 14 18];
