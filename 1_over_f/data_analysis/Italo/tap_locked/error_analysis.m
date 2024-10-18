0ccb503409e67b1eff033c9596d88ede0b75082f
ZHIBIN ZHOU <zhibinz2@uci.edu>
2024-10-17 18:04:44 -0700

    Create error_analysis.m

---
+++ 1_over_f/data_analysis/Italo/tap_locked/error_analysis.m
@@ -0,0 +1,10 @@
+cd /home/zhibinz2/Documents/GitHub/Motor_cordination/1_over_f/data_analysis/Italo/tap_locked
+load('errors.mat')
+load('ses12_seeds_cell_array.mat');
+ses12_seeds_cell_array=cell_data;
+load('ses12_start_stop_cell_array.mat')
+ses12_start_stop_cell_array=cell_data;
+load('ses12_joint_seq_cell_array.mat');
+ses12_joint_seq_cell_array=cell_data;
+
+%%
